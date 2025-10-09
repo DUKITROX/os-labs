4692020571454501d1dac0532742c78856a9be46
Green1705 <gorastgor@gmail.com>
2025-10-09 13:14:53 +0200

    Exercises 1 and 2

--- lab2/ejercicio2/read_strings.c
+++ lab2/ejercicio2/read_strings.c
@@ -16,12 +16,61 @@
  */
 char *loadstr(FILE *file)
 {
-	/* To be completed */
-	return NULL;	
+    if (!file) return NULL;
+
+    size_t cap = 64;
+    size_t len = 0;
+    char *buf = (char *)malloc(cap);
+    if (!buf) return NULL;
+
+    unsigned char ch;
+    for (;;) {
+        size_t n = fread(&ch, 1, 1, file);
+        if (n != 1) {
+            free(buf);
+            return NULL;
+        }
+        if (ch == '\0') {
+            //space for terminator and finish
+            if (len + 1 > cap) {
+                char *tmp = (char *)realloc(buf, len + 1);
+                if (!tmp) { free(buf); return NULL; }
+                 buf = tmp;
+            }
+            buf[len] = '\0';
+            return buf;
+        }
+        //making a bigger buffer if needed
+        if (len + 1 >= cap) {
+            size_t ncap = cap * 2;
+            char *tmp = (char *)realloc(buf, ncap);
+         if (!tmp) { free(buf); return NULL; }
+         cap = ncap;
+            buf = tmp;
+        }
+        buf[len++] = (char)ch;
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	/* To be completed */
-	return 0;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file_name>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "rb");
+    if (!file) {
+        err(2, "The input file %s could not be opened", argv[1]);
+    }
+
+    for (;;) {
+        char *str = loadstr(file);
+        if (!str) break;
+        printf("%s\n", str);
+        free(str);
+    }
+
+    fclose(file);
+    return 0;
 }
--- lab2/ejercicio2/write_strings.c
+++ lab2/ejercicio2/write_strings.c
@@ -5,6 +5,34 @@
 
 int main(int argc, char* argv[])
 {
-	/* To be completed */
-	return 0;
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <output_file> [strings ...]\n", argv[0]);
+        return 1;
+    }
+
+    const char *path = argv[1];
+    FILE *fp = fopen(path, "wb");
+    if (!fp) {
+        err(2, "The output file %s could not be opened", path);
+    }
+
+    for (int i = 2; i < argc; i++) {
+        const char *s = argv[i];
+        size_t len = strlen(s) + 1; 
+        const unsigned char *p = (const unsigned char *)s;
+        size_t written = 0;
+        while (written < len) {
+            size_t n = fwrite(p + written, 1, len - written, fp);
+            if (n == 0) {
+                fclose(fp);
+                err(3, "Error writing to %s", path);
+            }
+            written += n;
+        }
+    }
+
+    if (fclose(fp) != 0) {
+        err(4, "Error closing %s", path);
+    }
+    return 0;
 }
