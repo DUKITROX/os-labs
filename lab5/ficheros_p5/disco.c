d2c544a04cf78978ec265c8320255eacae202b53
Green1705 <gorastgor@gmail.com>
2025-12-14 12:45:01 +0100

    lab5 practice solve

--- lab5/ficheros_p5/disco.c
+++ lab5/ficheros_p5/disco.c
@@ -6,13 +6,52 @@
 #define CAPACITY 5
 #define VIPSTR(vip) ((vip) ? "  vip  " : "not vip")
 
-void enter_normal_client(int id)
-{
+typedef struct{
+		int id;
+		int isvip;
+	} info;
+
+pthread_mutex_t mutexcap;
+pthread_cond_t cv;
+int inside;
+int ticket_vip=0;
+int serving_vip=0;
+int ticket_normal=0;
+int serving_normal=0;
+
+
+void enter_normal_client(int id) {
+
+	pthread_mutex_lock(&mutexcap);
+	int ticket;
+	ticket = ticket_normal;
+	ticket_normal++;
+	
+	while (ticket != serving_normal || serving_vip != ticket_vip || inside >= CAPACITY){
+		pthread_cond_wait(&cv,&mutexcap);
+	}
+	inside++;
+	serving_normal++;
+	//printf("Normal client entering with id %d\n",id);
+	pthread_mutex_unlock(&mutexcap);
 
 }
 
-void enter_vip_client(int id)
-{
+void enter_vip_client(int id) {
+
+	pthread_mutex_lock(&mutexcap);
+	int ticket;
+	ticket = ticket_vip;
+	ticket_vip++;
+	
+	while (ticket != serving_vip || inside >= CAPACITY){
+		pthread_cond_wait(&cv,&mutexcap);
+	}
+	inside++;
+	serving_vip++;
+	//printf("Vip client entering with id %d \n",id);
+	pthread_mutex_unlock(&mutexcap);
+
 
 }
 
@@ -22,36 +61,82 @@ void dance(int id, int isvip)
 	sleep((rand() % 3) + 1);
 }
 
-void disco_exit(int id, int isvip)
-{
+void disco_exit(int id, int isvip) {
+	pthread_mutex_lock(&mutexcap);
+
+	inside--;
+	pthread_cond_broadcast(&cv);
+	pthread_mutex_unlock(&mutexcap);
 
 }
 
-void *client(void *arg)
-{
+void *client(void *arg) {
+
+	info *a = (info*) arg;
+	int id = a->id;
+	int isvip= a-> isvip;
+	free(a);
+
+	if (isvip){
+		enter_vip_client(id);
+	}
+	else{
+		enter_normal_client(id);
+	}
+	dance(id,isvip);
+	disco_exit(id,isvip);
+
+	return NULL;
 
 }
 
-int main(int argc, char *argv[])
-{
+
+
+int main(int argc, char *argv[]) {
+
+	
 	//getting the member data from the file
 	FILE* f;
 	int memnum=0;
+	pthread_mutex_init(&mutexcap,NULL);
+	pthread_cond_init(&cv,NULL);
+	
+	
 
 	f = fopen(argv[1], "r");
 	if (!f){
 		printf("Couldnt open file");
+		exit(1);
 	}
-
 	int fout=0;
 	int counter = 0;
-	while(fscanf(f,"%d",&fout) != EOF){
-		printf("%d\n",fout);
-		if (counter == 0) memnum = fout;
-		counter++;
 
+	while(fscanf(f,"%d",&fout) != EOF){
+		memnum = fout;
+		break;
 	}
 
+	pthread_t threads[memnum];
+	inside = 0;
+
+	while(fscanf(f,"%d",&fout) != EOF){
+		info* a = malloc(sizeof(*a));
+		a->id = counter;
+		a ->isvip = fout;
+		pthread_create(&threads[counter],NULL,client,a);
+		if (counter == memnum) break;
+		counter++;
+	
+	}
+
+
+	for (int i=0; i<memnum; i++){
+
+		pthread_join(threads[i],NULL);
+	}
+
+	pthread_mutex_destroy(&mutexcap);
+	pthread_cond_destroy(&cv);
 	
 
 	
